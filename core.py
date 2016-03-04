870233e3bae230fd24851fa1dc729fbd05d04103
Knikolskiy <you@example.com>
2016-03-04 14:50:25 +0300

    test client to connect added. not tested yet

--- core.py
+++ core.py
@@ -1,8 +1,9 @@
+
 from connections.telnet import get_authorized_connection
-from connections.tcp import SocketHandler
+from connections.sockserver import SocketHandler
 import settings
 import imports
-#TODO: Log handling (take it from my xlsx2xml and add log-leveling logic
+# TODO: Log handling (take it from my xlsx2xml and add log-leveling logic
 telnet_connect = get_authorized_connection(settings.HOST, settings.PASSWORD, settings.TELNET_PORT)
 tcp_connect = SocketHandler(settings.TCP_PORT, settings.SOCKET_CONTROL_LENGTH)
 
@@ -19,14 +20,13 @@ try:
             for parser in parsers:
                 parser.parse(line)
         request = tcp_connect.get_data_from_socket()
-        if request:
-            for asker in askers:
-                if asker.wait_for_response:
-                    asker.parse(line, telnet_connect)
-                else:
-                    command = asker.ask(request)
-                    if command:
-                        telnet_connect.write(command + '\n')
+        for asker in askers:
+            if asker.wait_for_response:
+                asker.parse(line, telnet_connect)
+            if request:
+                command = asker.ask(request)
+                if command:
+                    telnet_connect.write(command + '\n')
 
         
 except:
