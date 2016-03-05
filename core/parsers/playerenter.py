a99cb46dee74a69253ac2ecf1292c6c053ad0e58
Shpiler <sit_elis@mail.ru>
2016-03-05 22:18:58 +0300

    parser regexp fixed

--- core/parsers/playerenter.py
+++ core/parsers/playerenter.py
@@ -2,8 +2,8 @@ from core.workers import Parser
 
 
 class HeartBeatParser(Parser):
-    """parser for message when player enters. typical string = 'Player 76561198056103537/'Shpiler' logged in'"""
-    regexp = '*Player ?P<Id>/\'?P<Id>\' logged in'
+    """parser for message when player enters. typical string = '[PA] Player 76561198056103537/'Shpiler' logged in'"""
+    regexp = r"^\[..] Player (?P<ID>\d+)\/'(?P<name>\w+)' logged in$"
 
     def on_match(self, line, match_object, *args):
         print match_object.groupdict()
