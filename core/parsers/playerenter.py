618b48973ab9467daafa40f4f811a672667f075d
gluck <glucksistemi@gmail.com>
2016-03-06 07:08:05 +0300

    - peewee injected - models for peewee and django - DB saving from parsers

--- core/parsers/playerenter.py
+++ core/parsers/playerenter.py
@@ -1,9 +1,11 @@
 from core.workers import Parser
+from core.connections.database.handler import save_logon_event
 
 
 class HeartBeatParser(Parser):
     """parser for message when player enters. typical string = '[PA] Player 76561198056103537/'Shpiler' logged in'"""
-    regexp = r"^\[..] Player (?P<ID>\d+)\/'(?P<name>\w+)' logged in$"
+    regexp = r"^\[..] Player (?P<steam_id>\d+)\/'(?P<nick_name>\w+)' logged in$"
 
     def on_match(self, line, match_object, *args):
         print match_object.groupdict()
+        save_logon_event(1, **match_object.groupdict())
