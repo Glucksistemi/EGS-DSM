838622a44e09910b6a668287dbbbb433b4793b2c
gluck <glucksistemi@gmail.com>
2016-03-09 03:51:27 +0300

    logs. not tested

--- core/log.py
+++ core/log.py
@@ -1,16 +1,17 @@
 import datetime
 from core.settings import LOG_LEVELS, LOG_FILE
-
-levels = {
-    'error': 1,
-    'warn': 2,
-    'debug': 3
-}
+from core.connections.database.models import CoreLog
 
 class Log:
     file = None
     params = {}
-
+    levels = {}
+    lnums = {
+        'error': 1,
+        'warn': 2,
+        'debug': 3
+    }
+    
     def __init__(self):
         self.levels = LOG_LEVELS
         self.path = LOG_FILE
@@ -18,7 +19,14 @@ class Log:
             self.file = open(self.path.format(datetime.datetime.now().strftime('%Y%m%d-%H%M')), 'w')
 
     def log(self, level, logstr):
-        pass
+        if type(level) is str:
+            level = self.lnums[level]
+        if self.levels['file'] and self.levels['file'] >= level:
+            self.file.write(datetime.datetime.now().strftime('%Y%m%d-%H%M') + ' ' + logstr)
+        if self.levels['stdout'] and self.levels['stdout'] >= level:
+            print datetime.datetime.now().strftime('%Y%m%d-%H%M') + ' ' + logstr
+        if self.levels['database'] and self.levels['database'] >= level:
+            CoreLog.create()
 
     def close(self):
         self.file.close()
