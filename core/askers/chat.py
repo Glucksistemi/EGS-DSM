927b259680bc875347839596dc112136465f9e57
gluck <glucksistemi@gmail.com>
2016-03-27 16:14:47 +0300

    - basic css - server's messages now writes to db

--- core/askers/chat.py
+++ core/askers/chat.py
@@ -1,13 +1,14 @@
 from core.workers import Asker
+from core.connections.database.utils import save_chat_message
 
 
 class ChatAsker(Asker):
     request_name = 'say'
-    regexp = "^Message '(?P<msg>.*)' .*$"
+    regexp = r"^Message '(?P<msg>.*)' sent to all clients$"
 
     def on_ask(self, request):
         return "say '" + request.replace("'", "`") + "'", True
 
-    def on_response(self, match_object, response):
-
+    def on_response(self, line, result):
+        save_chat_message(message=result.groupdict()['msg'])
         return 'message sent', False
